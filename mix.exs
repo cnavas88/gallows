5afe6a7347cd1a93e490ea551279287319ca2726
Carlos Navas <dante.maniacs@gmail.com>
2020-08-22 09:05:07 +0200

    remove gettext dependency

--- mix.exs
+++ mix.exs
@@ -29,7 +29,6 @@ defmodule Gallows.Mixfile do
       {:phoenix_pubsub, "~> 1.0"},
       {:phoenix_html, "~> 2.10"},
       {:phoenix_live_reload, "~> 1.0", only: :dev},
-      # {:gettext, "~> 0.11"},
       {:cowboy, "~> 1.0"},
       {:plug_cowboy, "~> 1.0"},
 
